034c202543bc33fa7fc1e9e6e7ada6f2d15ac682
IharRubanik <igor.rubanik@gmail.com>
2022-04-19 00:41:25 +0400

    end social

--- script.js
+++ script.js
@@ -1,44 +1,64 @@
-const swiper = new Swiper('.flex-elem__slider', {
-    // Optional parameters
-    // effect: 'fade',
-    effect: 'coverflow',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+const swiper = new Swiper(".flex-elem__slider", {
+  // Optional parameters
+  // effect: 'fade',
+  effect: "coverflow",
+  loop: true,
 
-  const swiperMobile = new Swiper('.main-mobile__slider', {
-    // Optional parameters
-    direction: 'vertical',
-    // effect: 'fade',
-    // effect: 'coverflow',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.mobile-pagination',
-      clickable: true,
-    },
-  });
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 
-  let vh = window.innerHeight * 0.01;
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
+const swiperMobile = new Swiper(".main-mobile__slider", {
+  // Optional parameters
+  direction: "vertical",
+  // effect: 'fade',
+  // effect: 'coverflow',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".mobile-pagination",
+    clickable: true,
+  },
+});
+
+let vh = window.innerHeight * 0.01;
 // Затем устанавливаем значение свойства --vh
 // для корневого элемента
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+document.documentElement.style.setProperty("--vh", `${vh}px`);
 
-window.addEventListener('resize', () => {
-  // We execute the same script as before
+window.addEventListener("resize", () => {
+  // Выполняем тот же скрипт, что и раньше
   let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
 });
+
+
+// Собираем переменные
+let connection = document.querySelectorAll(".connection"),
+  sociaContainer = document.querySelector(".social-container"),
+  wrapper = document.querySelector(".wrapper");
+  // Проходим по массиву циклом
+for (let i = 0; i < connection.length; i++) {
+  // включаем событие клик на каждый элемент из массива
+  connection[i].onclick = function () {
+    // добавляем класс active
+    sociaContainer.classList.toggle("active");
+// Если у элемента есть класс active
+    if (sociaContainer.classList.contains("active")) {
+      // При клике удаляем его
+      sociaContainer.onclick = function () {
+        sociaContainer.classList.toggle("active");
+      };
+    }
+  };
+}
